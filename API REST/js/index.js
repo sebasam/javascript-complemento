4915058637c23cacc4d67ca45f60299f122a7d08
sebasam <sebasamayaxd@hotmail.com>
2026-05-13 20:52:29 -0500

    añadiendo paginacion y manejo de errores

--- API REST/js/index.js
+++ API REST/js/index.js
@@ -1,21 +1,55 @@
 const contenedor = document.getElementById('contenedor');
+const btnAnterior = document.getElementById('btnAnterior');
+const btnSiguiente = document.getElementById('btnSiguiente');
+let pagina = 1;
 
 const obtenerPersonajes = async() => {
     try {
         // funcion fetch trae una promesa y la esperamos con await
-        const respuesta = await fetch('https://rickandmortyapi.com/api/character');
+        const respuesta = await fetch(`https://rickandortyapi.com/api/character?page=${ pagina }`);
         const data = await respuesta.json();
-        console.log(data.results)
+        console.log(data.results);
+        contenedor.innerHTML = '';
         data.results.forEach(personaje => {
             contenedor.innerHTML += `
                 <div class="col-md-4 col-lg-3 text-center">
-                    <h2>${ personaje.name }</h2>
+                    <div class="card h-100">
+                        <img
+                            alt="${ personaje.name }"
+                            class="card-img-top"
+                            src="${ personaje.image }"
+                        >
+                        <div class="card-body>
+                            <h5>${ personaje.name }</h5>
+                            <p class="card-text">
+                                Estado: ${ personaje.status }
+                            </p>
+                            <p class="card-text">
+                                Especie: ${ personaje.species }                            
+                            </p>
+                        </div>
+                    </div>
                 </div>
             `;
         });
     } catch(error) {
         console.log(`Este es un error atrapado por el catch: ${ error }`);
+        contenedor.innerHTML = `
+            <span class="text-danger text-center">
+                Ocurrió un error al cargar los personajes
+            </span>
+        `;
     }
 };
+
+btnSiguiente.addEventListener('click', () => {
+    pagina++;
+    obtenerPersonajes();
+});
+
+btnAnterior.addEventListener('click', () => {
+    pagina--;
+    obtenerPersonajes();
+});
 
 obtenerPersonajes();
