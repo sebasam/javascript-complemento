b7a50803c823ded242ed44ed1268069a84119f01
sebasam <sebasamayaxd@hotmail.com>
2026-06-09 21:37:04 -0500

    angular initial project

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'tasks',
+        component: TasksComponent
+    },
+];
